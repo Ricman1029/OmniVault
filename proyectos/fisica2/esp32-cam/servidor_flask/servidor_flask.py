22f37131c9a48f34432214ed93408b697a36774b
Ricman <ricardofreccero@hotmail.com>
2025-06-02 04:28:36 -0300

    Me traje el servidor al repo por las dudas nomas

---
+++ proyectos/fisica2/esp32-cam/servidor_flask/servidor_flask.py
@@ -0,0 +1,16 @@
+# servidor_flask.py
+from flask import Flask, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def recibir_foto():
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"foto_{now}.jpg"
+    with open(filename, 'wb') as f:
+        f.write(request.data)
+    return "Imagen recibida", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
